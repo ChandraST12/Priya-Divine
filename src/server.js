ca86eb837aafae97f9d2d128387df7ea039578d1
Chandra Subhashini Thakran <91868226+ChandraST12@users.noreply.github.com>
2026-03-28 12:09:39 +0530

    debugging

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import app from "./app.js";
 import connectDB from "./config/db.js";
 dotenv.config();
-connectDB();
-
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-  console.log(`Server running on port ${PORT}`);
-});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((error)=>{
+  console.log("failed to connect db : ", error);
+  process.exit(1);
+})
